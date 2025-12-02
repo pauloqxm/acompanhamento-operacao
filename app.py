1a4ab871e20d72951eb09241791299d7999ed1ee
pauloqxm <pauloqxm@gmail.com>
2025-12-02 13:14:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import math
 import pandas as pd
 import numpy as np
 import streamlit as st
+import Fullscreen, MeasureControl
 from datetime import datetime, date
 from zoneinfo import ZoneInfo
 
@@ -12,6 +13,7 @@ import folium
 from folium import GeoJson, GeoJsonTooltip, LayerControl
 from folium.features import CustomIcon
 from streamlit_folium import st_folium
+from folium.plugins
 
 import altair as alt
 import streamlit.components.v1 as components
